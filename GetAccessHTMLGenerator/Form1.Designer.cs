5ce1c82679b6ade7ddfe2535576b2d18f5fb6aa6
asaf <asaf.tavor@gmail.com>
2020-03-21 16:23:37 +0200

    added version 1

---
+++ GetAccessHTMLGenerator/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace GetAccessHTMLGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.productName = new System.Windows.Forms.TextBox();
+            this.imageLink = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.description = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.generateHtmlButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Product Name:";
+            // 
+            // productName
+            // 
+            this.productName.Location = new System.Drawing.Point(109, 26);
+            this.productName.Name = "productName";
+            this.productName.Size = new System.Drawing.Size(309, 20);
+            this.productName.TabIndex = 1;
+            this.productName.TextChanged += new System.EventHandler(this.productName_TextChanged);
+            // 
+            // imageLink
+            // 
+            this.imageLink.Location = new System.Drawing.Point(109, 70);
+            this.imageLink.Name = "imageLink";
+            this.imageLink.Size = new System.Drawing.Size(309, 20);
+            this.imageLink.TabIndex = 3;
+            this.imageLink.TextChanged += new System.EventHandler(this.imageLink_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Image Link:";
+            // 
+            // description
+            // 
+            this.description.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.description.Location = new System.Drawing.Point(109, 120);
+            this.description.Multiline = true;
+            this.description.Name = "description";
+            this.description.Size = new System.Drawing.Size(309, 117);
+            this.description.TabIndex = 5;
+            this.description.TextChanged += new System.EventHandler(this.description_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 120);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Description:";
+            // 
+            // generateHtmlButton
+            // 
+            this.generateHtmlButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.generateHtmlButton.Enabled = false;
+            this.generateHtmlButton.Location = new System.Drawing.Point(454, 262);
+            this.generateHtmlButton.Name = "generateHtmlButton";
+            this.generateHtmlButton.Size = new System.Drawing.Size(157, 23);
+            this.generateHtmlButton.TabIndex = 6;
+            this.generateHtmlButton.Text = "Generate HTML";
+            this.generateHtmlButton.UseVisualStyleBackColor = true;
+            this.generateHtmlButton.Click += new System.EventHandler(this.generateHtmlButton_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(454, 26);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(157, 211);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(629, 312);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.generateHtmlButton);
+            this.Controls.Add(this.description);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.imageLink);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.productName);
+            this.Controls.Add(this.label1);
+            this.MinimumSize = new System.Drawing.Size(645, 350);
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(0, 0, 10, 10);
+            this.Text = "Get Aaaaaaaccess";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox productName;
+        private System.Windows.Forms.TextBox imageLink;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox description;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button generateHtmlButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ GetAccessHTMLGenerator/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+
+namespace GetAccessHTMLGenerator
+{
+    public partial class Form1 : Form
+    {
+
+        private bool productNameHasValue = false;
+        private bool imageLinkHasValue = false;
+        private bool descriptionHasValue = false;
+
+        private string productNameValue;
+        private string imageLinkValue;
+        private string descriptionValue;
+
+        private HtmlAgilityPack.HtmlDocument document;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void generateHtmlButton_Click(object sender, EventArgs e)
+        {
+            this.parseHTML();
+            this.copyHTMLToClipboard();
+        }
+
+        private void parseHTML()
+        {
+            string htmlFileName = @"html.html";
+            try
+            {
+                document = new HtmlAgilityPack.HtmlDocument();
+                document.Load(htmlFileName);
+            }
+            catch (System.IO.FileNotFoundException)
+            {
+                this.raiseAlert($"Failed to find '{htmlFileName}' in the directory!!!");
+                Load += (s, e) => Close();
+            }
+        }
+
+        private void copyHTMLToClipboard()
+        {
+            HtmlNode productNameTag = this.findNode("//h1[@id='productName']");
+            productNameTag.InnerHtml = this.productNameValue;
+
+            HtmlNode imgTag = this.findNode("//img[@id='productImage']");
+            imgTag.SetAttributeValue("src", this.imageLinkValue);
+
+            HtmlNode descriptionTag = this.findNode("//p[@id='description']");
+            descriptionTag.InnerHtml = this.descriptionValue;
+
+            Clipboard.SetText(this.document.DocumentNode.WriteTo());
+        }
+
+        private HtmlNode findNode(string xpath)
+        {
+            HtmlNodeCollection nodes = this.document.DocumentNode.SelectNodes(xpath);
+            if (nodes != null)
+                return nodes.First();
+            else
+            {
+                this.raiseAlert($"Failed to find xpath: '{xpath}'");
+                Application.Exit();
+                return null;
+            }
+
+        }
+
+        private void productName_TextChanged(object sender, EventArgs e)
+        {
+            this.updateVarValue(varToUpdate: out this.productNameValue, flagToUpdate: out this.productNameHasValue, value: productName.Text);
+            this.setGenerateHTMLButtonState();
+        }
+
+        private void imageLink_TextChanged(object sender, EventArgs e)
+        { 
+            this.updateVarValue(varToUpdate: out this.imageLinkValue, flagToUpdate: out this.imageLinkHasValue, value:imageLink.Text);
+            this.setGenerateHTMLButtonState();
+        }
+
+        private void description_TextChanged(object sender, EventArgs e)
+        {
+            this.updateVarValue(varToUpdate: out this.descriptionValue, flagToUpdate: out this.descriptionHasValue, value: description.Text);
+            this.setGenerateHTMLButtonState();
+        }
+
+        private void updateVarValue(out string varToUpdate, out bool flagToUpdate, string value)
+        {
+            
+            if (String.IsNullOrEmpty(value))
+            {
+                flagToUpdate = false;
+                varToUpdate = value;
+            }
+            else
+            {
+                flagToUpdate = true;
+                varToUpdate = value;
+            }
+        }
+
+        private void setGenerateHTMLButtonState()
+        {
+            generateHtmlButton.Enabled = this.descriptionHasValue && this.imageLinkHasValue && this.productNameHasValue;
+        }
+
+        private void raiseAlert(string message)
+        {
+            MessageBox.Show(message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+}
